bfc196e188eeed1b154e6e3a7016d81225b84689
heinegyo <heinegyo@gmail.com>
2020-09-21 21:25:15 +0800

    modify OrdersClass

--- back-end/src/com/company/store/domain/Orders.java
+++ back-end/src/com/company/store/domain/Orders.java
@@ -10,4 +10,36 @@ public class Orders {
     /*訂單狀態 1代表待確認 0代表已確認*/
     private int status = 1;
     private double total;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
 }
