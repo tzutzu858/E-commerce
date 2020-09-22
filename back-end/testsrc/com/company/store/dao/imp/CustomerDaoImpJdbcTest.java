de8670f477431eb2a8c02a4d5966b45f482543f1
heinegyo <heinegyo@gmail.com>
2020-09-22 14:54:15 +0800

    add CustomerDao test

--- back-end/testsrc/com/company/store/dao/imp/CustomerDaoImpJdbcTest.java
+++ back-end/testsrc/com/company/store/dao/imp/CustomerDaoImpJdbcTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerDaoImpJdbcTest {
@@ -35,6 +37,21 @@ class CustomerDaoImpJdbcTest {
 
     @Test
     void create() {
+        Customer customer = new Customer();
+        customer.setId("Mayday");
+        customer.setName("五月天");
+        customer.setPassword("maydaymayday");
+        customer.setPhone("0987654321");
+        customer.setBirthday(new Date(1111111111));
+
+        dao.create(customer);
+
+        Customer customer1 = dao.findByPk("Mayday");
+        assertEquals("Mayday",customer1.getId());
+        assertEquals("五月天",customer1.getName());
+        assertEquals("maydaymayday",customer1.getPassword());
+        assertEquals("0987654321",customer1.getPhone());
+        assertEquals(1111111111,customer1.getBirthday().getTime());
     }
 
     @Test
