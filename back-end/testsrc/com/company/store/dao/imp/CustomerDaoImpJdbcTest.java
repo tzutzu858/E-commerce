011acc12572e7ba81dcd5d5ba4fb21ebbbec4285
heinegyo <heinegyo@gmail.com>
2020-09-23 15:58:10 +0800

    carry out CustomerDaoImpJdbc test

--- back-end/testsrc/com/company/store/dao/imp/CustomerDaoImpJdbcTest.java
+++ back-end/testsrc/com/company/store/dao/imp/CustomerDaoImpJdbcTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,10 +30,17 @@ class CustomerDaoImpJdbcTest {
 
         assertNotNull(customer);
         assertEquals("google",customer.getId());
+        assertEquals("谷歌",customer.getName());
+        assertEquals("google",customer.getPassword());
+        assertEquals("09123456789",customer.getPhone());
+        assertEquals(1111111111,customer.getBirthday().getTime());
     }
 
     @Test
     void findAll() {
+        List<Customer> list = dao.findAll();
+        //這是測試總共有幾筆顧客資料
+        assertEquals(list.size(),6);
     }
 
     @Test
@@ -56,9 +64,29 @@ class CustomerDaoImpJdbcTest {
 
     @Test
     void modify() {
+        Customer customer = new Customer();
+
+        customer.setId("google");
+        customer.setName("估狗");
+        customer.setPassword("Google");
+        customer.setPhone("1234567890");
+        customer.setBirthday(new Date(20200923));
+
+        dao.modify(customer);
+
+        assertEquals("google",customer.getId());
+        assertEquals("估狗",customer.getName());
+        assertEquals("Google",customer.getPassword());
+        assertEquals("1234567890",customer.getPhone());
+        assertEquals(20200923,customer.getBirthday().getTime());
+
     }
 
     @Test
     void remove() {
+        dao.remove("Mayday");
+
+        Customer customer = dao.findByPk("Mayday");
+
     }
 }
