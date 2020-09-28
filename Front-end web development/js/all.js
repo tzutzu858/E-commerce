50ff6b36705b70c744f03932cdf8995700bea803
heinegyo <heinegyo@gmail.com>
2020-09-28 14:00:55 +0800

    finish mermbar

--- Front-end web development/js/all.js
+++ Front-end web development/js/all.js
@@ -25,7 +25,10 @@
 //   })
   // =================登入註冊頁面＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
   const signInForm = document.querySelector('.signIn-form');
+  var signInSpan = document.querySelector('.signIn-span');
   const signUpForm = document.querySelector('.signUp-form');
+  var signUpSpan =document.querySelector('.signUp-span');
+  
   // signIn.classList.toggle('signIn-session')
   
   /* ---------------登入和註冊 form 切換---------------------  */
@@ -33,16 +36,22 @@
   var signUpSwitch =document.querySelector('.sigUp-switch');
   signInSwitch.onclick=function(e){  
 	e.preventDefault();
-	signInForm.setAttribute('style','display: flex;')
-	signUpForm.setAttribute('style','display: none;')
+	signInForm.setAttribute('style','display: flex;');
+	signUpForm.setAttribute('style','display: none;');
+	signInSpan.setAttribute('style','display: none;');
+	signUpSpan.setAttribute('style','display: inline;');
+	
   }
   signUpSwitch.onclick=function(e){
 	  e.preventDefault();
-	  signInForm.setAttribute('style','display: none;')
-	  signUpForm.setAttribute('style','display: flex;')
+	  signInForm.setAttribute('style','display: none;');
+	  signUpForm.setAttribute('style','display: flex;');
+	  signInSpan.setAttribute('style','display: inline;');
+	  signUpSpan.setAttribute('style','display: none;');
+	  
   }
   //----------------檢查註冊表單----------------------------
-  function verify(myform) {
+  function signUpverify(myform) {
               var errorMes = "";
               if (myform.userid.value == "") {
                   errorMes += "請填寫帳號!\n";
@@ -72,103 +81,18 @@
                   return false;
               }
           }
-var submitBtn = document.querySelector('.submit-signIn');
-	submitBtn.onclick=function(e){
-		e.preventDefault();
-		}
-//-----------------檢查登入表單-------------------------
-// function verify(myform) {
-//               var errorMes = "";
-//               if (myform.userid.value == "") {
-//                   errorMes += "請填寫帳號!\n";
-//               }
-//               if (myform.password.value == "") {
-//                   errorMes += "請填寫密碼!\n";
-//               }
-//               if (myform.password.value != myform.password2.value) {
-//                   errorMes += "密碼不一致!\n";
-//               }
-// 			  if(myform.phone.value == ""){
-// 				  errorMes += "請填寫電話!\n";
-// 			  }
-  
-//               var pattern = /^((((19|20)(([02468][048])|([13579][26]))-02-29))|((20[0-9][0-9])|(19[0-9][0-9]))-((((0[1-9])|(1[0-2]))-((0[1-9])|(1\d)|(2[0-8])))|((((0[13578])|(1[02]))-31)|(((0[1,3-9])|(1[0-2]))-(29|30)))))$/;
-  
-//               if (!pattern.test(myform.birthday.value)) {
-//                   errorMes += "出生日期格式錯誤！\n";
-//               } 
-//               if (errorMes == "") {
-//                   return true;
-//               } else {
-//                   alert(errorMes);
-//                   return false;
-//               }
-//           }
-//test		  
-  // for (i = 0; i < signInBtn.length; i++) {
-  //     signInBtn[i].addEventListener('click', () => {
-  //         if (document.querySelector('.signIn-session')) {
-  //             signIn.classList.toggle('signIn-session')
-  //             register.classList.toggle('register-section')
-  //         }
-  //     });
-  // }
-  
-  
-  // const signUpBtn = document.querySelectorAll('.sigUp-button');
-  
-  // for (i = 0; i < signUpBtn.length; i++) {
-  //     signUpBtn[i].addEventListener('click', () => {
-  //         if (document.querySelector('.register-section')) {
-  //             signIn.classList.toggle('signIn-session')
-  //             register.classList.toggle('register-section')
-  //         }
-  //     });
-  // }
-  
-  
-  
-  /*  -----------------檢查 input 是否填寫
-  ---------------------如果沒有填寫完整就不送表單出去  */
-  
-  // const formSignIn = document.querySelector('.signIn-form');
-  // const formRegister = document.querySelector('.register-form');
-  // formRegister.addEventListener('submit',
-  //     (e) => {
-  //         console.log("register_ok")
-  //         if (!(checkInputIsOk(formRegister))) {
-  //             e.preventDefault();
-  //         }
-  //     });
-  
-  //     formSignIn.addEventListener('submit',
-  //     (e) => {
-  //         console.log("signIn_ok")
-  //         if (!(checkInputIsOk(formSignIn))) {
-  //             e.preventDefault();
-  //         }
-  //     });
-  
-  
-  // function checkInputIsOk(form) {
-  //     const formBlockInput = form.querySelectorAll('.form_block_input');
-  //     console.log(formBlockInput);
-  //     var hasCheck = true;
-  //     for (const inputName of formBlockInput) {
-  //         const errP = inputName.querySelector('p');
-  //         const inputValue = inputName.querySelector('input');
-  //         console.log(inputValue.value);
-  //         if (inputValue.value) {
-  //             errP.className = 'input-ok';
-              
-  //         } else {
-  //             errP.className = 'input-err';
-  //             hasCheck = false;
-  //         }
-  //     }
-  
-  //     if (hasCheck) {
-  //         return true;
-  //     }
-  //     return false;
-  // }
+  function signIpverify(myform) {
+              var errorMes = "";
+              if (myform.userid.value == "") {
+                  errorMes += "請輸入帳號!\n";
+              }
+              if (myform.password.value == "") {
+                  errorMes += "請輸入密碼!\n";
+              }
+              if (errorMes == "") {
+                  return true;
+              } else {
+                  alert(errorMes);
+                  return false;
+              }
+          }
